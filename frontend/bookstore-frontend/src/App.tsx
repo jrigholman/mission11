551a0c2233fecb45bbe370596506d9ba0fc5a349
jonat <jonat@JONS-SURFACE>
2025-04-07 01:02:31 -0600

    first

---
+++ frontend/bookstore-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+// Jon Holman
+
+import './App.css';
+import BookList from "./components/BookList";
+
+function App() {
+  return (
+    <div className="App">
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
+
